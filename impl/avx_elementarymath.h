20dcebfb27667ecaf5ee2c4bac51b755e4db5e21
zeept <zeeptobean@gmail.com>
2023-11-13 18:47:33 +0700

    add avx elementary math and general support

---
+++ impl/avx_elementarymath.h
@@ -0,0 +1,31 @@
+#include <../base.h>
+
+/**/
+
+__m256 _mm256_rcp_ps(__m256 a) {
+    __m256 r;
+    r.hi = _mm_rcp_ps(a.hi);
+    r.lo = _mm_rcp_ps(a.lo);
+    return r;
+}
+
+__m256 _mm256_rsqrt_ps(__m256 a) {
+    __m256 r;
+    r.hi = _mm_rsqrt_ps(a.hi);
+    r.lo = _mm_rsqrt_ps(a.lo);
+    return r;
+}
+
+__m256d _mm256_sqrt_pd(__m256d a) {
+    __m256d r;
+    r.hi = _mm_sqrt_pd(a.hi);
+    r.lo = _mm_sqrt_pd(a.lo);
+    return r;
+}
+
+__m256 _mm256_sqrt_ps(__m256 a) {
+    __m256 r;
+    r.hi = _mm_sqrt_ps(a.hi);
+    r.lo = _mm_sqrt_ps(a.lo);
+    return r;
+}
---
+++ impl/avx_generalsupport.h
@@ -0,0 +1,28 @@
+#include <../base.h>
+
+/**/
+
+__m256d _mm256_undefined_pd(void) {
+    __m256d r;
+    return r;
+}
+
+__m256 _mm256_undefined_ps(void) {
+    __m256 r;
+    return r;
+}
+
+__m256i _mm256_undefined_sị6(void) {
+    __m256i r;
+    return r;
+}
+
+//no idea how to emulate this, even on resetting sse registers
+void _mm256_zeroall(void) {
+
+}
+
+//no upper part to even clear...
+void _mm256_zeroupper(void) {
+
+}
