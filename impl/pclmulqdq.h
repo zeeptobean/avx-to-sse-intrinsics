0b62ec668ce4fe298b017b7f6be233b2e31458f6
zeept_ubuntu <zeeptobean@gmail.com>
2023-11-17 20:01:27 +0700

    completely overhaul testing; add impl3 for pclmulqdq and internal clmul function

--- impl/pclmulqdq.h
+++ impl/pclmulqdq.h
@@ -1,4 +1,5 @@
 #include "../base.h"
+#include "internal_func.h"
 
 /**/
 
@@ -235,6 +236,34 @@ __m128i _mm_clmulepi64_si128_zp_impl2(__m128i __a, __m128i __b, const int imm8) 
     cond = op2 & (1LL << 63);
     lo ^= (op1 << 63);
     hi ^= (op1 >> 1);
+
+    return _mm_set_epi64x(hi, lo);
+}
+
+__m128i _mm_clmulepi64_si128_zp_impl3(__m128i __a, __m128i __b, const int imm8) {
+    uint64_t a[2], b[2];
+    uint64_t lo = 0, hi = 0, op1 = 0, op2 = 0;
+
+    _mm_store_si128((__m128i*) &a, __a);
+    _mm_store_si128((__m128i*) &b, __b);
+
+    op1 = a[imm8 & 1];
+    op2 = b[(imm8 >> 4) & 1];
+
+    uint32_t op1lo = (uint32_t) op1;
+    uint32_t op1hi = op1 >> 32;
+    uint32_t op2lo = (uint32_t) op2;
+    uint32_t op2hi = op2 >> 32;
+    uint64_t tmp = 0;
+
+    lo = zp_internal_clmul32(op1lo, op2lo);
+    hi = zp_internal_clmul32(op1hi, op2hi);
+    tmp = zp_internal_clmul32(op1hi, op2lo);
+    lo ^= (tmp << 32);
+    hi ^= (tmp >> 32);
+    tmp = zp_internal_clmul32(op1lo, op2hi);
+    lo ^= (tmp << 32);
+    hi ^= (tmp >> 32);
 
     return _mm_set_epi64x(hi, lo);
 }
--- internal_func.h
+++ internal_func.h
@@ -9,3 +9,13 @@ void zp_internal_umul128(uint64_t x, uint64_t y, uint64_t *lo, uint64_t *hi) {
     *hi = (uint64_t) (result >> 64);
 #endif
 }
+
+uint64_t zp_internal_clmul32(uint32_t a, uint32_t b) {
+    uint64_t ret = 0;
+    for(int i=0; i < 32; i++) {
+        if(b & (1 << i)) {
+            ret ^= (((uint64_t) a) << i);
+        }
+    }
+    return ret;
+}
