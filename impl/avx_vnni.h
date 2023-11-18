d4adcb85b62b56798e896ef886ece77409c331e1
zeept_ubuntu <zeeptobean@gmail.com>
2023-11-18 14:29:35 +0700

    add avx_vnni

---
+++ impl/avx_vnni.h
@@ -0,0 +1,194 @@
+#include "../base.h"
+#include "../internal_func.h"
+
+/**/
+
+__m128i _mm_dpbusd_epi32(__m128i src, __m128i __a, __m128i __b) {
+    //Must case sign -> unsign to zero-extend, otherwise sign-extend 
+    //In this case the type are specified then no more cast
+    uint8_t a[16];
+    int8_t b[16];
+    _mm_storeu_si128( (__m128i*) a, __a);
+    _mm_storeu_si128( (__m128i*) b, __b);
+
+    __m128i aele[4], bele[4];
+
+    //Load to lane
+
+    aele[0] = _mm_set_epi32(a[12], a[8], a[4], a[0]);
+    aele[1] = _mm_set_epi32(a[13], a[9], a[5], a[1]);
+    aele[2] = _mm_set_epi32(a[14], a[10], a[6], a[2]);
+    aele[3] = _mm_set_epi32(a[15], a[11], a[7], a[3]);
+
+    bele[0] = _mm_set_epi32(b[12], b[8], b[4], b[0]);
+    bele[1] = _mm_set_epi32(b[13], b[9], b[5], b[1]);
+    bele[2] = _mm_set_epi32(b[14], b[10], b[6], b[2]);
+    bele[3] = _mm_set_epi32(b[15], b[11], b[7], b[3]);
+
+    //16bit intermediate product still stay within low 32bit
+    aele[0] = _mm_mullo_epi32(aele[0], bele[0]); 
+    aele[1] = _mm_mullo_epi32(aele[1], bele[1]); 
+    aele[2] = _mm_mullo_epi32(aele[2], bele[2]); 
+    aele[3] = _mm_mullo_epi32(aele[3], bele[3]); 
+
+    src = _mm_add_epi32(src, aele[0]);
+    src = _mm_add_epi32(src, aele[1]);
+    src = _mm_add_epi32(src, aele[2]);
+    src = _mm_add_epi32(src, aele[3]);
+
+    return src;
+}
+
+__m256i _mm256_dpbusd_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpbusd_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpbusd_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+__m128i _mm_dpbusd_avx_epi32(__m128i src, __m128i a, __m128i b) {
+    return _mm_dpbusd_epi32(src, a, b);
+}
+
+__m256i _mm256_dpbusd_avx_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpbusd_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpbusd_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+/**/
+
+__m128i _mm_dpbusds_epi32(__m128i src, __m128i __a, __m128i __b) {
+    uint8_t a[16];
+    int8_t b[16];
+    _mm_storeu_si128( (__m128i*) a, __a);
+    _mm_storeu_si128( (__m128i*) b, __b);
+
+    __m128i aele[4], bele[4];
+
+    aele[0] = _mm_set_epi32(a[12], a[8], a[4], a[0]);
+    aele[1] = _mm_set_epi32(a[13], a[9], a[5], a[1]);
+    aele[2] = _mm_set_epi32(a[14], a[10], a[6], a[2]);
+    aele[3] = _mm_set_epi32(a[15], a[11], a[7], a[3]);
+
+    bele[0] = _mm_set_epi32(b[12], b[8], b[4], b[0]);
+    bele[1] = _mm_set_epi32(b[13], b[9], b[5], b[1]);
+    bele[2] = _mm_set_epi32(b[14], b[10], b[6], b[2]);
+    bele[3] = _mm_set_epi32(b[15], b[11], b[7], b[3]);
+
+    aele[0] = _mm_mullo_epi32(aele[0], bele[0]); 
+    aele[1] = _mm_mullo_epi32(aele[1], bele[1]); 
+    aele[2] = _mm_mullo_epi32(aele[2], bele[2]); 
+    aele[3] = _mm_mullo_epi32(aele[3], bele[3]); 
+
+    src = zp_internal_paddsd(src, aele[0]);
+    src = zp_internal_paddsd(src, aele[1]);
+    src = zp_internal_paddsd(src, aele[2]);
+    src = zp_internal_paddsd(src, aele[3]);
+
+    return src;
+}
+
+__m256i _mm256_dpbusds_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpbusds_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpbusds_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+__m128i _mm_dpbusds_avx_epi32(__m128i src, __m128i a, __m128i b) {
+    return _mm_dpbusds_epi32(src, a, b);
+}
+
+__m256i _mm256_dpbusds_avx_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpbusds_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpbusds_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+/**/
+
+__m128i _mm_dpwssd_epi32(__m128i src, __m128i __a, __m128i __b) {
+    uint16_t a[8], b[8];
+    _mm_storeu_si128( (__m128i*) a, __a);
+    _mm_storeu_si128( (__m128i*) b, __b);
+
+    __m128i aele[2], bele[2];
+
+    aele[0] = _mm_set_epi32(a[6], a[4], a[2], a[0]);
+    aele[1] = _mm_set_epi32(a[7], a[5], a[3], a[1]);
+
+    bele[0] = _mm_set_epi32(b[6], b[4], b[2], b[0]);
+    bele[1] = _mm_set_epi32(b[7], b[5], b[3], b[1]);
+
+    aele[0] = _mm_mullo_epi32(aele[0], bele[0]); 
+    aele[1] = _mm_mullo_epi32(aele[1], bele[1]); 
+
+    src = _mm_add_epi32(src, aele[0]);
+    src = _mm_add_epi32(src, aele[1]);
+
+    return src;
+}
+
+__m256i _mm256_dpwssd_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpwssd_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpwssd_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+__m128i _mm_dpwssd_avx_epi32(__m128i src, __m128i a, __m128i b) {
+    return _mm_dpwssd_epi32(src, a, b);
+}
+
+__m256i _mm256_dpwssd_avx_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpwssd_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpwssd_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+/**/
+
+__m128i _mm_dpwssds_epi32(__m128i src, __m128i __a, __m128i __b) {
+    uint16_t a[8], b[8];
+    _mm_storeu_si128( (__m128i*) a, __a);
+    _mm_storeu_si128( (__m128i*) b, __b);
+
+    __m128i aele[2], bele[2];
+
+    aele[0] = _mm_set_epi32(a[6], a[4], a[2], a[0]);
+    aele[1] = _mm_set_epi32(a[7], a[5], a[3], a[1]);
+
+    bele[0] = _mm_set_epi32(b[6], b[4], b[2], b[0]);
+    bele[1] = _mm_set_epi32(b[7], b[5], b[3], b[1]);
+
+    aele[0] = _mm_mullo_epi32(aele[0], bele[0]); 
+    aele[1] = _mm_mullo_epi32(aele[1], bele[1]); 
+
+    src = zp_internal_paddsd(src, aele[0]);
+    src = zp_internal_paddsd(src, aele[1]);
+
+    return src;
+}
+
+__m256i _mm256_dpwssds_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpwssds_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpwssds_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
+__m128i _mm_dpwssds_avx_epi32(__m128i src, __m128i a, __m128i b) {
+    return _mm_dpwssds_epi32(src, a, b);
+}
+
+__m256i _mm256_dpwssds_avx_epi32(__m256i src, __m256i a, __m256i b) {
+    __m256i r;
+    r.hi = _mm_dpwssds_epi32(src.hi, a.hi, b.hi);
+    r.lo = _mm_dpwssds_epi32(src.lo, a.lo, b.lo);
+    return r;
+}
+
--- internal_func.h
+++ internal_func.h
@@ -19,3 +19,26 @@ uint64_t zp_internal_clmul32(uint32_t a, uint32_t b) {
     }
     return ret;
 }
+
+//Packed saturated signed 32bit add based on this answer https://stackoverflow.com/a/56544654
+__m128i zp_internal_paddsd(__m128i a, __m128i b) {
+    const __m128i int_max = _mm_set1_epi32(0x7FFFFFFF);
+
+    // normal result
+    __m128i res = _mm_add_epi32(a, b);
+
+    // If result saturates, it has the same sign as both a and b
+    __m128i sign_bit = _mm_srli_epi32(a, 31); // shift sign to lowest bit
+    __m128i saturated = _mm_add_epi32(int_max, sign_bit);
+
+    // saturation happened if inputs do not have different signs, 
+    // but sign of result is different:
+    __m128i sign_xor  = _mm_xor_si128(a, b);
+    __m128i overflow = _mm_andnot_si128(sign_xor, _mm_xor_si128(a,res));
+
+    return _mm_castps_si128(_mm_blendv_ps(_mm_castsi128_ps(res),
+                                          _mm_castsi128_ps(saturated),
+                                          _mm_castsi128_ps(overflow) 
+                                          )
+                            );
+}
