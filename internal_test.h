73b3cb783072d19aa8ce07f7e9be7975ae052efe
zeept <zeeptobean@gmail.com>
2023-11-24 21:04:03 +0700

    edit xoshiro256** signature, add internal general number randomizer

---
+++ internal_test.h
@@ -0,0 +1,165 @@
+#ifndef zeept_avx_testheader_incl
+#define zeept_avx_testheader_incl
+
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "internal_xoshiro256starstar.h"
+
+//Generate between [lo, hi]
+double zp_gen_double(int32_t lo, int32_t hi) {
+    if(lo > hi) {
+        int32_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    char str[100];
+    double f;
+    int32_t intpart = lo + (rand_next() % (hi-lo+1));
+    uint32_t decimalpart = rand_next();
+    if(snprintf(str, 100, "%d.%u", intpart, decimalpart) % 7 == 3) {
+        rand_long_jump();
+    }
+    sscanf(str, "%lf", &f);
+    if(decimalpart % (intpart >> 9) == 2) {
+        rand_jump();
+    }
+    return f;
+}
+
+//Generate between [lo, hi]
+float zp_gen_float(int16_t lo, int16_t hi) {
+    if(lo > hi) {
+        int16_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    char str[100];
+    float f;
+    int16_t intpart = lo + (rand_next() % (hi-lo+1));
+    uint32_t decimalpart = rand_next();
+    if(snprintf(str, 100, "%hd.%u", intpart, decimalpart) % 7 == 3) {
+        rand_long_jump();
+    }
+    sscanf(str, "%f", &f);
+    if(decimalpart % (intpart >> 9) == 2) {
+        rand_jump();
+    }
+    if(rand_next() % 131 == 101) rand_jump();
+    return f;
+}
+
+//Generate between [lo, hi)
+uint64_t zp_gen_uint64(uint64_t lo, uint64_t hi) {
+    if(lo > hi) {
+        uint64_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    uint64_t ran = lo + (rand_next() % (hi-lo));
+    if(ran % 2113 == 344) rand_long_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+int64_t zp_gen_int64(int64_t lo, int64_t hi) {
+    if(lo > hi) {
+        int64_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    int64_t ran = lo + (rand_next() % (hi-lo));
+    if(ran % 413 == 1) rand_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+uint32_t zp_gen_uint32(uint32_t lo, uint32_t hi) {
+    if(lo > hi) {
+        uint32_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    uint32_t ran = lo + (rand_next() % (hi-lo));
+    if(ran % 9113 == 341) rand_long_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+int32_t zp_gen_int32(int32_t lo, int32_t hi) {
+    if(lo > hi) {
+        int32_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    int32_t ran = lo + (rand_next() % (hi-lo));
+    if(ran % 23 == 4) rand_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+uint16_t zp_gen_uint16(uint16_t lo, uint16_t hi) {
+    if(lo > hi) {
+        uint16_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    uint16_t ran = lo + (rand_next() % (hi-lo));
+    if(rand_next() % 11577 == 31) rand_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+int16_t zp_gen_int16(int16_t lo, int16_t hi) {
+    if(lo > hi) {
+        int16_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    int16_t ran = lo + (rand_next() % (hi-lo));
+    if(ran % 17 == 1) rand_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+uint8_t zp_gen_uint8(uint8_t lo, uint8_t hi) {
+    if(lo > hi) {
+        uint8_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    uint8_t ran = lo + (rand_next() % (hi-lo));
+    if(rand_next() % rand_next() == 23) rand_long_jump();
+    return ran;
+}
+
+//Generate between [lo, hi)
+int8_t zp_gen_int8(int8_t lo, int8_t hi) {
+    if(lo > hi) {
+        int8_t temp = lo;
+        lo = hi;
+        hi = temp; 
+    }
+
+    rand_long_jump();
+    int8_t ran = lo + (rand_next() % (hi-lo));
+    if(rand_next() % 3331 == rand_next() % 39) rand_jump();
+    return ran;
+}
+
+#endif
--- internal_xoshiro256starstar.h
+++ internal_xoshiro256starstar.h
@@ -1,16 +1,6 @@
 #ifndef zp_internal_XOSHIRO256SS
 #define zp_internal_XOSHIRO256SS
 
-/*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
-
-To the extent possible under law, the author has dedicated all copyright
-and related and neighboring rights to this software to the public domain
-worldwide. This software is distributed without any warranty.
-
-See <http://creativecommons.org/publicdomain/zero/1.0/>. */
-
-/*Modified*/
-
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
@@ -21,7 +11,7 @@ static inline uint64_t rotl(const uint64_t x, int k) {
 
 static uint64_t s[4];
 
-void init_seed(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
+void rand_init_seed(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
     s[0] = a;
     s[1] = b;
     s[2] = c;
@@ -29,17 +19,17 @@ void init_seed(uint64_t a, uint64_t b, uint64_t c, uint64_t d) {
 }
 
 /*Worse way to init. Not deterministic*/
-void init_seed2(uint64_t a) {
+void rand_init_seed2(uint64_t a) {
     s[0] = a;
     s[1] = rotl(s[0], 23) ^ s[0];
     uint64_t tmp = (s[1] * 11515253) + 3451;
-    tmp = ((rand() * 179) + 31) ^ tmp;
-    s[2] = rotl(tmp ^ rand(), 13);
-    tmp = ((rand() * 19) + 2331) ^ tmp;
+    tmp = ((tmp * 179) + 31) ^ tmp;
+    s[2] = rotl(tmp ^ (uint64_t) rand(), 13);
+    tmp = (( (uint64_t) rand() * 19) + 2331) ^ tmp;
     s[3] = s[2] ^ tmp;
 }
 
-uint64_t next(void) {
+uint64_t rand_next(void) {
 	const uint64_t result = rotl(s[1] * 5, 7) * 9;
 
 	const uint64_t t = s[1] << 17;
@@ -61,7 +51,7 @@ uint64_t next(void) {
    to 2^128 calls to next(); it can be used to generate 2^128
    non-overlapping subsequences for parallel computations. */
 
-void jump(void) {
+void rand_jump(void) {
 	static const uint64_t JUMP[] = { 0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c };
 
 	uint64_t s0 = 0;
@@ -76,7 +66,7 @@ void jump(void) {
 				s2 ^= s[2];
 				s3 ^= s[3];
 			}
-			next();	
+			rand_next();	
 		}
 		
 	s[0] = s0;
@@ -92,7 +82,7 @@ void jump(void) {
    from each of which jump() will generate 2^64 non-overlapping
    subsequences for parallel distributed computations. */
 
-void long_jump(void) {
+void rand_long_jump(void) {
 	static const uint64_t LONG_JUMP[] = { 0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241, 0x39109bb02acbe635 };
 
 	uint64_t s0 = 0;
@@ -107,7 +97,7 @@ void long_jump(void) {
 				s2 ^= s[2];
 				s3 ^= s[3];
 			}
-			next();	
+			rand_next();	
 		}
 		
 	s[0] = s0;
