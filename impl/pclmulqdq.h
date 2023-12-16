d4f75b682e9e680f5629c2c579545ae6bbc695da
zeept@laptop <zeept@laptop>
2023-12-16 11:19:09 +0700

    add marco for pclmul

--- impl/pclmulqdq.h
+++ impl/pclmulqdq.h
@@ -8,19 +8,28 @@ namespace zp {
 
 /*Declaration*/
 
-__m128i _mm_clmulepi64_si128(__m128i __a, __m128i __b, const int imm8);
-zp::__m256i _mm256_clmulepi64_epi128(zp::__m256i __a, zp::__m256i __b, const int imm8);
+#ifndef zeept_disable_marco_function
+#define _mm_clmulepi64_si128(a, b, imm8) 
+#endif
+
+template <uint32_t imm8>
+__m128i _mm_clmulepi64_si128_cpp(__m128i __a, __m128i __b);
+
+template <uint32_t imm8>
+zp::__m256i _mm256_clmulepi64_epi128_cpp(zp::__m256i __a, zp::__m256i __b);
 
 /*Implementation*/
 
-zp::__m256i _mm256_clmulepi64_epi128(zp::__m256i __a, zp::__m256i __b, const int imm8) {
+template <uint32_t imm8>
+zp::__m256i _mm256_clmulepi64_epi128_cpp(zp::__m256i __a, zp::__m256i __b) {
     zp::__m256i r;
-    r.lo = _mm_clmulepi64_si128(__a.lo, __b.lo, imm8);
-    r.hi = _mm_clmulepi64_si128(__a.hi, __b.hi, imm8);
+    r.lo = _mm_clmulepi64_si128_cpp<imm8>(__a.lo, __b.lo);
+    r.hi = _mm_clmulepi64_si128_cpp<imm8>(__a.hi, __b.hi);
     return r;
 }
 
-__m128i _mm_clmulepi64_si128(__m128i __a, __m128i __b, const int imm8) {
+template <uint32_t imm8>
+__m128i _mm_clmulepi64_si128_cpp(__m128i __a, __m128i __b) {
     uint64_t a[2], b[2];
     uint64_t lo = 0, hi = 0, op1 = 0, op2 = 0;
 
@@ -31,9 +40,9 @@ __m128i _mm_clmulepi64_si128(__m128i __a, __m128i __b, const int imm8) {
     op2 = b[(imm8 >> 4) & 1];
 
     uint32_t op1lo = (uint32_t) op1;
-    uint32_t op1hi = op1 >> 32;
+    uint32_t op1hi = (uint32_t) (op1 >> 32);
     uint32_t op2lo = (uint32_t) op2;
-    uint32_t op2hi = op2 >> 32;
+    uint32_t op2hi = (uint32_t) (op2 >> 32);
     uint64_t tmp = 0;
 
     lo = zp_internal_clmul32(op1lo, op2lo);
